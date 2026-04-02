edaf9ad6db83dcac15ff436657b853183996a9f1
sauravnirala <sauravnirala44@gmail.com>
2026-04-03 02:33:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Calculator API is running"
+    return render_template("index.html")
 
 @app.route("/add", methods=["GET"])
 def add():
